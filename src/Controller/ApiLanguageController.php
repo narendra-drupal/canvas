85d428d05d4cd60abb85e1a548e278aafb4d1742
Ted Bowman <ted+git@tedbow.com>
2026-04-17 16:56:17 -0400

    add ApiLanguageController for language dropdown

---
+++ src/Controller/ApiLanguageController.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\canvas\Controller;
+
+use Drupal\Core\Cache\CacheableJsonResponse;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+
+/**
+ * HTTP API for language information.
+ *
+ * @internal This HTTP API is intended only for the Canvas UI. These controllers
+ *   and associated routes may change at any time.
+ */
+final class ApiLanguageController {
+
+  public function __construct(
+    private readonly LanguageManagerInterface $languageManager,
+  ) {}
+
+  /**
+   * Returns a list of configurable languages set up on the site.
+   */
+  public function list(): CacheableJsonResponse {
+    $languages = $this->languageManager->getLanguages(LanguageInterface::STATE_CONFIGURABLE);
+
+    $data = [];
+    foreach ($languages as $language) {
+      $data[] = [
+        'id' => $language->getId(),
+        'name' => $language->getName(),
+        'direction' => $language->getDirection(),
+        'isDefault' => $language->isDefault(),
+      ];
+    }
+
+    $cacheability = new CacheableMetadata();
+    $cacheability->addCacheTags(['config:configurable_language_list']);
+    $response = new CacheableJsonResponse($data);
+    $response->addCacheableDependency($cacheability);
+    return $response;
+  }
+
+}
