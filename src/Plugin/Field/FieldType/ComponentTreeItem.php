22fd5f3beacfa7540a0484e3117cef8d49e7beae
Ted Bowman <ted+git@tedbow.com>
2026-05-04 14:33:33 -0400

    strip non-translatable input props in entity_translation_create hook rather than item pre-save

--- src/Plugin/Field/FieldType/ComponentTreeItem.php
+++ src/Plugin/Field/FieldType/ComponentTreeItem.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace Drupal\canvas\Plugin\Field\FieldType;
 
 use Drupal\canvas\Entity\ComponentTreeEntityInterface;
-use Drupal\canvas\Plugin\DataType\ComponentInputs;
 use Drupal\canvas\PropSource\PropSource;
 use Drupal\Component\Plugin\DependentPluginInterface;
 use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Block\MessagesBlockPluginInterface;
 use Drupal\Core\Block\TitleBlockPluginInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
-use Drupal\Core\Entity\TranslatableInterface;
 use Drupal\Core\Entity\TypedData\EntityDataDefinition;
 use Drupal\Core\Field\Attribute\FieldType;
 use Drupal\Core\Field\FieldDefinitionInterface;
@@ -625,22 +623,6 @@ class ComponentTreeItem extends FieldItemBase {
     // that the logic of this field type is correct.
     if ($input_values === NULL && $source->requiresExplicitInput()) {
       throw new \LogicException(\sprintf('Missing input for component instance with UUID %s', $component_instance_uuid));
-    }
-
-    // For non-default translations, strip non-translatable input keys so that
-    // only translatable values are stored per-language. Non-translatable values
-    // are merged from the default translation at read time.
-    // @see \Drupal\canvas\ComponentSource\ComponentSourceBase::getExplicitInput()
-    // @see \Drupal\canvas\ComponentSource\ComponentSourceBase::validateComponentInput()
-    // @see https://www.drupal.org/project/canvas/issues/3583684
-    $parent_list = $this->getParent();
-    \assert($parent_list instanceof ComponentTreeItemList);
-    if ($parent_list->isTreeTranslationSynced() && !$parent_list->isInputsTranslationSynced() && $entity instanceof TranslatableInterface && !$entity->isDefaultTranslation() && $input_values !== NULL) {
-      $inputs_typed_data = $this->get('inputs');
-      \assert($inputs_typed_data instanceof ComponentInputs);
-      $translatable_keys = $inputs_typed_data->getTranslatableInputKeys();
-      $filtered_inputs = \array_intersect_key($input_values, \array_flip($translatable_keys));
-      $this->setInput($filtered_inputs);
     }
 
     $this->optimizeInputs();
